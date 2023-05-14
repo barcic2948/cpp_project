5250c8db22f0585e6a4da87a68333dd23d56d6d0
barcic2948 <barcic2948@gmail.com>
2023-05-14 22:42:17 +0200

    Fixes

--- src/game.h
+++ src/game.h
@@ -15,7 +15,8 @@ class Game : public GenericGame {
         const unsigned int window_width = 1000;
         const unsigned int window_height = 700;
         sf::Font font;
-        const std::string font_path = "/home/ltafker/Desktop/code/cpp_project/fonts/CenturyGothic.ttf";
+        //const std::string font_path = "/home/ltafker/Desktop/code/cpp_project/fonts/CenturyGothic.ttf";
+        const std::string font_path = "C:\\Users\\barci\\Desktop\\code\\cpp_project\\fonts\\CenturyGothic.ttf";
 
         sf::Color *gate_color = new sf::Color(76, 76, 76, 255);
         sf::Color *text_color = new sf::Color(254, 254, 254, 255);
